5cabd58aae7a3c553adae5ce6ad594d20d3f41e6
reactdevs23 <dalimkumar212582@gmail.com>
2025-06-07 20:34:30 +0600

    alias solved

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react(), eslint(), tsconfigPaths()],
-  server: {
-    allowedHosts: ["civas-macbook-pro.siberian-alioth.ts.net"],
+  plugins: [react(), eslint()],
+  resolve: {
+    alias: {
+      "@": new URL("./src", import.meta.url).pathname, // ✅ ESM-friendly alias
+    },
   },
 });
